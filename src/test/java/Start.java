86048d100ee7b501b4cfcc7cf0347a323dbd0671
julia-kliot <juliakliot.jk@gmail.com>
2022-09-04 19:48:13 +0300

    start trello

---
+++ src/test/java/Start.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Start {
+
+WebDriver wd;
+
+@Test
+    public void startTrello(){
+
+    //System.setProperty("webdriver","C:/Users/julia/Downloads/chromedriver.exe");
+    wd=new ChromeDriver();
+    wd.navigate().to("https://trello.com/");
+    wd.navigate().back();
+    wd.navigate().forward();
+    wd.navigate().refresh();
+
+    wd.close();
+    wd.quit();
+
+}
+}
